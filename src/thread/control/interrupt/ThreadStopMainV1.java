1041caae82d5698a58bbd5f8f1feaf6b928d2766
hj <parkhj062@gmail.com>
2025-12-14 00:07:41 +0900

    thread stop by flag

---
+++ src/thread/control/interrupt/ThreadStopMainV1.java
@@ -0,0 +1,30 @@
+package thread.control.interrupt;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class ThreadStopMainV1 {
+
+    public static void main(String[] args) {
+        MyTask myTask = new MyTask();
+        Thread thread = new Thread(myTask);
+        thread.start();
+
+        sleep(4000);
+        myTask.runFlag = false;
+    }
+
+    static class MyTask implements Runnable {
+
+        volatile boolean runFlag = true;
+
+        @Override
+        public void run() {
+            while (runFlag) {
+                log("working..");
+                sleep(3000);
+            }
+            log("end");
+        }
+    }
+}
