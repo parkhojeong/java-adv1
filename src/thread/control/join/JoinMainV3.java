070f413307f0b83917e40506517fb2c91fad48ac
hj <parkhj062@gmail.com>
2025-12-13 23:42:27 +0900

    total by join

---
+++ src/thread/control/join/JoinMainV3.java
@@ -0,0 +1,44 @@
+package thread.control.join;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class JoinMainV3 {
+    public static void main(String[] args) throws InterruptedException {
+        Job task1 = new Job(1, 50);
+        Job task2 = new Job(51, 100);
+        Thread thread1 = new Thread(task1);
+        Thread thread2 = new Thread(task2);
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+        int sum = task1.result + task2.result;
+        log(sum);
+
+    }
+
+    static class Job implements Runnable {
+        int start;
+        int end;
+        int result;
+
+        public Job(int start, int end) {
+            this.start = start;
+            this.end = end;
+        }
+
+        public int getResult() {
+            return result;
+        }
+
+        @Override
+        public void run() {
+            sleep(2000);
+            for (int i = start; i <= end; i++) {
+                result += i;
+            }
+        }
+    }
+}
