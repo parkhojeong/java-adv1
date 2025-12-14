f5463fe4f809c6157ce14fca943900db8ebca1ab
hj <parkhj062@gmail.com>
2025-12-14 22:53:05 +0900

    count with volatile

---
+++ src/thread/volatile1/VolatileCountMain.java
@@ -0,0 +1,34 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileCountMain {
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        t.start();
+
+        sleep(1000);
+        task.flag = false;
+        log("flag=" + task.flag + ", count=" + task.count + "main");
+        log("main end");
+    }
+
+    static class MyTask implements Runnable {
+        volatile boolean flag = true;
+        volatile long count;
+
+        @Override
+        public void run() {
+            while (flag) {
+                count++;
+                if(count % 100_000_000 == 0){
+                    log("flag=" + flag + ", count=" + count + "in while");
+                }
+            }
+            log("flag=" + flag + ", count=" + count + "while end");
+
+        }
+    }
+}
