8c91d0f3b2b21d0ce9821c344adc7de938325fe0
hj <parkhj062@gmail.com>
2025-12-14 22:34:13 +0900

    cpu cache memory problem(memory visibility)

---
+++ src/thread/volatile1/VolatileFlagMain.java
@@ -0,0 +1,32 @@
+package thread.volatile1;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class VolatileFlagMain {
+
+    public static void main(String[] args) {
+        MyTask task = new MyTask();
+        Thread t = new Thread(task, "work");
+        log("runFlag=" + task.runFlag);
+        t.start();
+
+        sleep(1000);
+        task.runFlag = false;
+        log("runFlag=" + task.runFlag);
+        log("main end");
+    }
+
+    static class MyTask implements Runnable {
+        boolean runFlag = true;
+
+        @Override
+        public void run() {
+            log("task start");
+            while (runFlag) {
+
+            }
+            log("task end");
+        }
+    }
+}
