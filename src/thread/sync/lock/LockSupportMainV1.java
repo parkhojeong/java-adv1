bde19fa061735cfdcab8a3cbd75087ef9b98f290
hj <parkhj062@gmail.com>
2025-12-15 20:19:31 +0900

    LockSupport1
    park(): RUNNABLE -> WAITING
    unpark(): WAITING -> RUNNABLE
    park -> interrupt: RUNNABLE -> WAITING -> RUNNABLE(interrupted true)

---
+++ src/thread/sync/lock/LockSupportMainV1.java
@@ -0,0 +1,30 @@
+package thread.sync.lock;
+
+import java.util.concurrent.locks.LockSupport;
+
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class LockSupportMainV1 {
+    public static void main(String[] args) {
+        Thread thread = new Thread(new ParkTest(), "t1");
+        thread.start();
+
+        sleep(100);
+        log("t1 state = " + thread.getState());
+
+        log("main -> unpark t1");
+        LockSupport.unpark(thread);
+//        thread.interrupt();
+    }
+
+    static class ParkTest implements Runnable {
+        @Override
+        public void run() {
+            log("park start");
+            LockSupport.park();
+            log("park end, state = " + Thread.currentThread().getState());
+            log("interrupted = " + Thread.currentThread().isInterrupted());
+        }
+    }
+}
