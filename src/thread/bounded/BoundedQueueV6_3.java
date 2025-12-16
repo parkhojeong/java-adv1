914fd543df0bf7adc881b2cc75ec587fef1ec47b
hj <parkhj062@gmail.com>
2025-12-16 20:53:57 +0900

    Producer Consumer problem - Blocking queue take,poll with time

---
+++ src/thread/bounded/BoundedQueueV6_3.java
@@ -0,0 +1,41 @@
+package thread.bounded;
+
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.TimeUnit;
+
+import static util.MyLogger.log;
+
+public class BoundedQueueV6_3 implements BoundedQueue{
+
+    private BlockingQueue<String> queue;
+
+    public BoundedQueueV6_3(int max) {
+        this.queue = new ArrayBlockingQueue<>(max);
+    }
+
+    @Override
+    public void put(String data) {
+        boolean offer = false;
+        try {
+            offer = queue.offer(data, 1, TimeUnit.NANOSECONDS);
+            log("offer = " + offer);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String take() {
+        try {
+            return queue.poll(2, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return queue.toString();
+    }
+}
