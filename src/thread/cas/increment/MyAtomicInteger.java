3343b0c07922d49604b51700c3374ed734d8d9b7
hj <parkhj062@gmail.com>
2025-12-16 21:39:11 +0900

    atomic operation problem - AtomicInteger

---
+++ src/thread/cas/increment/MyAtomicInteger.java
@@ -0,0 +1,17 @@
+package thread.cas.increment;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class MyAtomicInteger implements IncrementInteger{
+    private AtomicInteger value = new AtomicInteger(0);
+
+    @Override
+    public void increment() {
+        value.incrementAndGet();
+    }
+
+    @Override
+    public int get() {
+        return value.get();
+    }
+}
