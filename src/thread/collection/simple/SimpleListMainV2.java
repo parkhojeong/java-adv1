d6eb469be6014bbd9f59a17f737a8140519a4e3d
parkhojeong <parkhj062@gmail.com>
2025-12-17 02:43:26 +0900

    SimpleList v2 - multi thread problem

---
+++ src/thread/collection/simple/SimpleListMainV2.java
@@ -0,0 +1,41 @@
+package thread.collection.simple;
+
+import static util.MyLogger.log;
+
+public class SimpleListMainV2 {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        test(new BasicList());
+    }
+
+    private static void test(SimpleList list) throws InterruptedException {
+        log(list.getClass().getSimpleName());
+
+        Runnable addA = new Runnable() {
+            @Override
+            public void run() {
+                list.add("A");
+                log("t1: list.add(A)");
+            }
+        };
+        Runnable addB = new Runnable() {
+            @Override
+            public void run() {
+                list.add("B");
+                log("t1: list.add(B)");
+            }
+        };
+
+        Thread t1 = new Thread(addA, "t1");
+        Thread t2 = new Thread(addB, "t2");
+
+        t1.start();
+        t2.start();
+
+        t1.join();
+        t2.join();
+
+        log(list);
+    }
+}
