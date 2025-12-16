9daff8f19a4e16b8f1ab667fc570ff2f5fd3e615
hj <parkhj062@gmail.com>
2025-12-16 20:55:48 +0900

    Producer Consumer problem - Blocking queue add,remove with exception

--- src/thread/bounded/BoundedMain.java
+++ src/thread/bounded/BoundedMain.java
@@ -7,7 +7,7 @@ import static util.ThreadUtils.sleep;
 
 public class BoundedMain {
     public static void main(String[] args) {
-        BoundedQueue queue = new BoundedQueueV6_3(2);
+        BoundedQueue queue = new BoundedQueueV6_4(2);
 
         producerFirst(queue);
 //        consumerFirst(queue);
