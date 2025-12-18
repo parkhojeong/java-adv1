c26ea759cceb0c55358f52a2d92044d325056e96
hj <parkhj062@gmail.com>
2025-12-18 20:27:13 +0900

    [Executor 전략 - 캐시 풀 전략]

---
+++ src/thread/executor/poolsize/PoolSizeMainV3.java
@@ -0,0 +1,34 @@
+package thread.executor.poolsize;
+
+import thread.executor.RunnableTask;
+
+import java.util.concurrent.*;
+
+import static thread.executor.ExecutorUtils.printState;
+import static util.MyLogger.log;
+import static util.ThreadUtils.sleep;
+
+public class PoolSizeMainV3 {
+    public static void main(String[] args) {
+        ExecutorService es = new ThreadPoolExecutor(0, Integer.MAX_VALUE,
+                2, TimeUnit.SECONDS,
+                new SynchronousQueue<Runnable>());
+
+        log("pool 생성");
+        printState(es);
+
+        for (int i = 1; i <= 6; i++) {
+            String taskName = "task" + i;
+            es.execute(new RunnableTask(taskName));
+            printState(es, taskName);
+        }
+
+        printState(es);
+
+        sleep(3000);
+        printState(es);
+
+        es.close();
+        log("== shutdown 완료 ==");
+    }
+}
